8df4635f5a220a8fc7e956a3174ddbc2aa862f41
Pr5do <github.v5x7y@aleeas.com>
2022-07-04 12:32:08 -0300

    Added derivatives on backpropagation method

--- simpleNeuralNetwork.py
+++ simpleNeuralNetwork.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from math import sqrt
 from random import randrange
 
 
@@ -13,14 +14,14 @@ class NeuralNetwork:
     ):
         self.learning_rate = learning_rate
 
-        self.input_layer = np.zeros(neurouns_on_input_layer)
+        self.input_layer = np.zeros((neurouns_on_input_layer))
         self.hidden_layer = np.zeros(neurouns_on_hidden_layer)
         self.output_layer = np.zeros(neurouns_on_output_layer)
 
         self.weights_input_layer_to_hidden_layer = np.random.rand(
             neurouns_on_input_layer, neurouns_on_hidden_layer
         )
-        self.weights_layer_to_output_layer = np.random.rand(
+        self.weights_hidden_layer_to_output_layer = np.random.rand(
             neurouns_on_hidden_layer, neurouns_on_output_layer
         )
 
@@ -38,14 +39,17 @@ class NeuralNetwork:
         expected_values[0][x] = 1
         return expected_values
 
-    def reLU(self, x):
-        return np.maximum(0, x)
+    def reLU(self, x, derivative=False):
+        if derivative:
+            return x > 0
+        else:
+            return np.maximum(0, x)
 
-    def sigmoid(self, x):
+    def sigmoid(self, x, derivative=False):
+        if derivative:
+            derivative = False
+            return self.sigmoid(x)*(1-self.sigmoid(x))
         return 1 / (1 + np.exp(-x))
-
-    def derivative_of_sigmoid(self, x):
-        return self.sigmoid(x)*(1-self.sigmoid(x))
 
     def loss_function(self, predicted_value, actual_value):
         return np.sum(np.subtract(predicted_value, actual_value)**2)
@@ -55,49 +59,84 @@ class NeuralNetwork:
         return e_x / e_x.sum(axis=0)
 
     def forward_propagation(self, x):
-        x = np.array(x)
         self.input_layer = x + self.input_layer
 
         self.hidden_layer_without_activity = np.dot(
             self.input_layer, self.weights_input_layer_to_hidden_layer
-        )
+        ) + self.bias_on_hidden_layer.T
 
         self.hidden_layer = self.sigmoid(
-            self.hidden_layer_without_activity + self.bias_on_hidden_layer.T
+            self.hidden_layer_without_activity
         )
 
         self.output_layer_without_activity = np.dot(
-            self.hidden_layer, self.weights_layer_to_output_layer
-        )
+            self.hidden_layer, self.weights_hidden_layer_to_output_layer
+        ) + self.bias_on_output_layer.T
+
         self.output_layer = self.sigmoid(
-            self.output_layer_without_activity + self.bias_on_output_layer.T
+            self.output_layer_without_activity
         )
         return self.output_layer
 
-    def backpropagation(self, x, y, mini_batch_size=500):
+    def backpropagation(self, x_samples, y_samples):
+        vector_of_desired_changes = np.array([])
+        i = 0
+        while i != len(y_samples):
+            predicted_value = self.forward_propagation(x_samples[i])
+            cost_value = self.loss_function(predicted_value.T, y_samples[i])
+            print(f"Predicted Value: {np.argmax(predicted_value)} ")
+            print(f"Actual Value: {y_samples[i]} ")
+
+            self.derivative_W2 = 2*sqrt(cost_value)*self.sigmoid(
+                self.output_layer_without_activity, derivative=True
+            ).T*self.hidden_layer
+
+            self.derivative_b2 = 2*sqrt(cost_value)*self.sigmoid(
+                self.output_layer_without_activity, derivative=True
+            ).T
+            print(self.input_layer.shape)
+
+            self.derivative_W1 = 2*sqrt(cost_value)*self.sigmoid(
+                self.output_layer_without_activity, derivative=True
+            )*self.weights_hidden_layer_to_output_layer*self.sigmoid(
+                    self.hidden_layer_without_activity, derivative=True
+                ).T*self.input_layer
+
+            self.derivative_b1 = 2*sqrt(cost_value)*self.sigmoid(
+                self.output_layer_without_activity, derivative=True
+            ).T*self.sigmoid(
+                    self.hidden_layer_without_activity, derivative=True
+                )
+
+            # desired_change = np.subtract(cost_value, self.expected_value(y_samples[i]))
+            # vector_of_desired_changes = np.append(vector_of_desired_changes, desired_change)
+            i += 1
+        # return vector_of_desired_changes
+
+    def gradient_descent(self):
+        self.weights_hidden_layer_to_output_layer = self.weights_hidden_layer_to_output_layer - self.learning_rate*self.derivative_W2
+        self.bias_on_output_layer = self.bias_on_output_layer - self.learning_rate*self.derivative_b2
+        self.weights_input_layer_to_hidden_layer = self.weights_input_layer_to_hidden_layer - self.learning_rate*self.derivative_W1
+        self.bias_on_hidden_layer = self.bias_on_hidden_layer - self.learning_rate*self.derivative_b1
+
+    def train(self, x, y, mini_batch_size=500):
         x, y = np.array(x), np.array(y)
-        epoch = 1
         x_samples = np.array_split(x, mini_batch_size)
         y_samples = np.array_split(y, mini_batch_size)
-        vector_of_desired_changes = np.array([])
+        epoch = 1
         for x, y in zip(x_samples, y_samples):
+            print(f"Epoch: {epoch}")
             epoch += 1
-            predicted_value = self.forward_propagation(x)
-            cost_value = self.loss_function(predicted_value.T, y)
-            desired_change = np.subtract(cost_value, self.expected_value(y))
-            vector_of_desired_changes = np.append(vector_of_desired_changes, desired_change)
-        return vector_of_desired_changes
+            self.backpropagation(x, y)
+            self.gradient_descent()
+            break
 
-    def gradient_descent(self, x, y):
-        raise NotImplementedError
-
-    def train(self):
         raise NotImplementedError
 
 
 if __name__ == "__main__":
-    train_data = pd.read_csv("./datasets/MNIST/train.csv")
-    test_data = pd.read_csv("./datasets/MNIST/test.csv")
+    train_data = pd.read_csv("./dataset/MNIST/train.csv")
+    test_data = pd.read_csv("./dataset/MNIST/test.csv")
 
     train_data = train_data.sample(frac=1, random_state=1).reset_index()
 
@@ -106,4 +145,4 @@ if __name__ == "__main__":
 
     nl = NeuralNetwork(0.01)
 
-    print(len(nl.backpropagation(X_train, y_train)))
+    nl.train(X_train, y_train)
