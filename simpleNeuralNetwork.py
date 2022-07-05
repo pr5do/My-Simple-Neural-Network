Trying to fix the gradient descent and the dimensionality of the derivatives of the weights and biases

simpleNeuralNetwork.py
@@ -1,43 +1,33 @@
 import numpy as np
 import pandas as pd
-from math import sqrt
-from random import randrange
 
 
 class NeuralNetwork:
     def __init__(
         self,
-        learning_rate,
         neurouns_on_input_layer=785,
         neurouns_on_hidden_layer=30,
         neurouns_on_output_layer=10,
     ):
-        self.learning_rate = learning_rate
 
-        self.input_layer = np.zeros((neurouns_on_input_layer))
-        self.hidden_layer = np.zeros(neurouns_on_hidden_layer)
-        self.output_layer = np.zeros(neurouns_on_output_layer)
+        self.hidden_layer = np.zeros((neurouns_on_hidden_layer, 1))
+        self.output_layer = np.zeros((neurouns_on_output_layer, 1))
 
         self.weights_input_layer_to_hidden_layer = np.random.rand(
-            neurouns_on_input_layer, neurouns_on_hidden_layer
+             neurouns_on_input_layer, neurouns_on_hidden_layer
         )
         self.weights_hidden_layer_to_output_layer = np.random.rand(
             neurouns_on_hidden_layer, neurouns_on_output_layer
         )
 
-        self.bias_on_hidden_layer = np.full(
-            (neurouns_on_hidden_layer, 1), randrange(1, 10)
-        )
-
-        self.bias_on_output_layer = np.full(
-            (neurouns_on_output_layer, 1), randrange(1, 10)
-
-        )
+        self.bias_on_hidden_layer = np.random.rand(neurouns_on_hidden_layer, 1)
+        self.bias_on_output_layer = np.random.rand(neurouns_on_output_layer, 1)
 
     def expected_value(self, x):
-        expected_values = np.zeros((1, 10))
-        expected_values[0][x] = 1
-        return expected_values
+        expected_value = np.zeros((10, 1))
+        expected_value[x] = 1
+        expected_value = expected_value.T
+        return expected_value
 
     def reLU(self, x, derivative=False):
         if derivative:
@@ -51,78 +41,105 @@ class NeuralNetwork:
             return self.sigmoid(x)*(1-self.sigmoid(x))
         return 1 / (1 + np.exp(-x))
 
-    def loss_function(self, predicted_value, actual_value):
-        return np.sum(np.power(np.subtract(predicted_value, actual_value), 2))
+    def loss_function(self, predicted_value, actual_value, derivative=False):
+        if derivative:
+            return np.sum(2*np.subtract(predicted_value, actual_value))
+        else:
+            return np.sum(np.power(np.subtract(predicted_value, actual_value), 2))
 
     def softmax(self, x):
         e_x = np.exp(x - np.max(x))
         return e_x / e_x.sum(axis=0)
 
     def forward_propagation(self, x):
-        self.input_layer = x + self.input_layer
 
-        self.hidden_layer_without_activity = np.dot(
-            self.input_layer, self.weights_input_layer_to_hidden_layer
-        ) + self.bias_on_hidden_layer.T
+        self.hidden_layer_without_activity = self.weights_input_layer_to_hidden_layer.T.dot(x.T) + self.bias_on_hidden_layer
 
         self.hidden_layer = self.sigmoid(
             self.hidden_layer_without_activity
         )
 
-        self.output_layer_without_activity = np.dot(
-            self.hidden_layer, self.weights_hidden_layer_to_output_layer
-        ) + self.bias_on_output_layer.T
+        self.output_layer_without_activity = self.weights_hidden_layer_to_output_layer.T.dot(
+            self.hidden_layer
+        ) + self.bias_on_output_layer
 
         self.output_layer = self.sigmoid(
             self.output_layer_without_activity
         )
+
         return self.output_layer
 
-    def backpropagation(self, x_samples, y_samples):
-        i = 0
-        predicted_value = self.forward_propagation(x_samples[i])
-        cost_value = self.loss_function(predicted_value.T, self.expected_value(y_samples[i]))
+    def backpropagation(self, x, y):
+        predicted_value = self.forward_propagation(x)
+        cost_value_derivative = self.loss_function(
+                predicted_value.T, self.expected_value(y), derivative=True
+            )
+        print(f"{'-*-'*15} PREDICTION {'-*-'*15}")
+        print(f"Predicted Value: {np.argmax(predicted_value)}")
+        print(f"Actual Value: {y}")
+        print(f"{'-*-'*15}{'-*-'*19}")
 
-        derivative_W2 = 2*np.sqrt(cost_value)*self.sigmoid(
-            self.output_layer_without_activity, derivative=True
-        ).T*self.hidden_layer_without_activity
+        derivative_W2 = (cost_value_derivative*self.sigmoid(
+            self.output_layer_without_activity, derivative=True)
+        ).dot(self.hidden_layer.T).T
 
-        derivative_b2 = 2*np.sqrt(cost_value)*self.sigmoid(
-            self.output_layer_without_activity, derivative=True
+        print(f"Derivative_W2: {derivative_W2.shape}, weights_hidden_layer_to_output_layer: {self.weights_hidden_layer_to_output_layer.shape}")
+        assert derivative_W2.shape == self.weights_hidden_layer_to_output_layer.shape
+
+        derivative_b2 = (cost_value_derivative*(self.sigmoid(
+                self.output_layer_without_activity, derivative=True).T
+        )).T
+
+        print(f"Derivative_b2: {derivative_b2.shape}, bias_on_output_layer: {self.bias_on_output_layer.shape}")
+        assert derivative_b2.shape == self.bias_on_output_layer.shape
+
+        derivative_b1 = cost_value_derivative*self.sigmoid(
+            self.output_layer_without_activity.T, derivative=True
+        ).dot(self.weights_hidden_layer_to_output_layer.T).dot(
+            self.sigmoid(self.hidden_layer_without_activity, derivative=True)
         )
+        print(f"Derivative_b1: {derivative_b1.shape}, bias_on_hidden_layer: {self.bias_on_hidden_layer.shape}")
 
-        derivative_W1 = 2*np.sqrt(cost_value)*self.sigmoid(
-            self.output_layer_without_activity, derivative=True
-        )*self.weights_hidden_layer_to_output_layer*self.sigmoid(
-                self.hidden_layer_without_activity, derivative=True).T
+        assert derivative_b1.shape == self.bias_on_hidden_layer.shape
 
-
-        derivative_b1 = 2*np.sqrt(cost_value)*self.sigmoid(
-            self.output_layer_without_activity, derivative=True
-        ).T*self.sigmoid(
+        derivative_W1 = cost_value_derivative*self.sigmoid(
+            self.output_layer_without_activity.T, derivative=True
+        ).dot(self.weights_hidden_layer_to_output_layer.T).dot(self.sigmoid(
                 self.hidden_layer_without_activity, derivative=True)
+        ).dot(x)
 
-        i += 1
+        print(f"Derivative_W1: {derivative_W1.shape}, weights_input_layer_to_hidden_layer: {self.weights_input_layer_to_hidden_layer.shape}")
+        assert derivative_W1.shape == self.weights_input_layer_to_hidden_layer.shape
+
         return derivative_W2, derivative_b2, derivative_W1, derivative_b1
 
-    def gradient_descent(self, derivative_W2, derivative_b2, derivative_W1, derivative_b1):
-        self.weights_hidden_layer_to_output_layer = np.subtract(self.weights_hidden_layer_to_output_layer.T, (self.learning_rate*derivative_W2))
-        self.bias_on_output_layer = np.subtract(self.bias_on_output_layer, self.learning_rate*derivative_b2)
-        self.bias_on_hidden_layer = np.subtract(self.bias_on_hidden_layer, self.learning_rate*derivative_b1)
-        self.weights_input_layer_to_hidden_layer = np.subtract(self.weights_input_layer_to_hidden_layer, self.learning_rate*derivative_W1)
-        return True
+    def gradient_descent(
+            self,
+            derivative_W2,
+            derivative_b2,
+            derivative_W1,
+            derivative_b1,
+            learning_rate):
 
-    def train(self, x, y, mini_batch_size=500):
+        self.weights_hidden_layer_to_output_layer -= learning_rate*derivative_W2
+
+        self.weights_input_layer_to_hidden_layer -= learning_rate*derivative_W1
+        self.bias_on_hidden_layer -= learning_rate*derivative_b1
+
+        self.bias_on_output_layer -= learning_rate*derivative_b2
+
+    def train(self, x, y, learning_rate, mini_batch_size=500):
         x, y = np.array(x), np.array(y)
         x_samples = np.array_split(x, mini_batch_size)
         y_samples = np.array_split(y, mini_batch_size)
         epoch = 1
-        for x, y in zip(x_samples, y_samples):
+        for x_sample, y_sample in zip(x_samples, y_samples):
             print(f"Epoch: {epoch}")
             epoch += 1
-            derivative_W2, derivative_b2, derivative_W1, derivative_b1 = self.backpropagation(x, y)
-            self.gradient_descent(derivative_W2, derivative_b2, derivative_W1, derivative_b1)
-            break
+            for x, y in zip(x_sample, y_sample):
+                x = np.reshape(x, (1, 785))
+                derivative_W2, derivative_b2, derivative_W1, derivative_b1 = self.backpropagation(x, y)
+                self.gradient_descent(derivative_W2, derivative_b2, derivative_W1, derivative_b1, learning_rate)
 
 
 if __name__ == "__main__":
@@ -134,6 +151,6 @@ if __name__ == "__main__":
     X_train = train_data.drop("label", axis=1)
     y_train = train_data.label
 
-    nl = NeuralNetwork(0.01)
+    nl = NeuralNetwork()
 
-    nl.train(X_train, y_train)
+    nl.train(X_train, y_train, 0.01)