Gradient descent fixed and working with at least one sample of data

simpleNeuralNetwork.py
@@ -26,7 +26,7 @@ class NeuralNetwork:
     def expected_value(self, x):
         expected_value = np.zeros((10, 1))
         expected_value[x] = 1
-        expected_value = expected_value.T
+        # expected_value = expected_value.T
         return expected_value
 
     def reLU(self, x, derivative=False):
@@ -53,62 +53,32 @@ class NeuralNetwork:
 
     def forward_propagation(self, x):
 
-        self.hidden_layer_without_activity = self.weights_input_layer_to_hidden_layer.T.dot(x.T) + self.bias_on_hidden_layer
+        self.hidden_layer_without_activity = np.dot(self.weights_input_layer_to_hidden_layer.T, x.T) + self.bias_on_hidden_layer
 
-        self.hidden_layer = self.sigmoid(
-            self.hidden_layer_without_activity
-        )
+        self.hidden_layer = self.sigmoid(self.hidden_layer_without_activity)
 
-        self.output_layer_without_activity = self.weights_hidden_layer_to_output_layer.T.dot(
-            self.hidden_layer
-        ) + self.bias_on_output_layer
+        self.output_layer_without_activity = np.dot(self.weights_hidden_layer_to_output_layer.T, self.hidden_layer) + self.bias_on_output_layer
 
-        self.output_layer = self.sigmoid(
-            self.output_layer_without_activity
-        )
+        self.output_layer = self.sigmoid(self.output_layer_without_activity)
 
         return self.output_layer
 
     def backpropagation(self, x, y):
         predicted_value = self.forward_propagation(x)
-        cost_value_derivative = self.loss_function(
-                predicted_value.T, self.expected_value(y), derivative=True
-            )
-        print(f"{'-*-'*15} PREDICTION {'-*-'*15}")
-        print(f"Predicted Value: {np.argmax(predicted_value)}")
-        print(f"Actual Value: {y}")
-        print(f"{'-*-'*15}{'-*-'*19}")
+        cost_value = self.loss_function(predicted_value.T, self.expected_value(y))
+        print(f"Cost Value: {cost_value}")
 
-        derivative_W2 = (cost_value_derivative*self.sigmoid(
-            self.output_layer_without_activity, derivative=True)
-        ).dot(self.hidden_layer.T).T
+        m = y.size
 
-        print(f"Derivative_W2: {derivative_W2.shape}, weights_hidden_layer_to_output_layer: {self.weights_hidden_layer_to_output_layer.shape}")
+        derivative_Z2 = predicted_value - self.expected_value(y)
+        derivative_W2 = 1/m * np.dot(derivative_Z2, self.hidden_layer.T).T
         assert derivative_W2.shape == self.weights_hidden_layer_to_output_layer.shape
-
-        derivative_b2 = (cost_value_derivative*(self.sigmoid(
-                self.output_layer_without_activity, derivative=True).T
-        )).T
-
-        print(f"Derivative_b2: {derivative_b2.shape}, bias_on_output_layer: {self.bias_on_output_layer.shape}")
+        derivative_b2 = 1/m * np.sum(derivative_Z2, axis=1, keepdims=True)
         assert derivative_b2.shape == self.bias_on_output_layer.shape
-
-        derivative_b1 = cost_value_derivative*self.sigmoid(
-            self.output_layer_without_activity.T, derivative=True
-        ).dot(self.weights_hidden_layer_to_output_layer.T).dot(
-            self.sigmoid(self.hidden_layer_without_activity, derivative=True)
-        )
-        print(f"Derivative_b1: {derivative_b1.shape}, bias_on_hidden_layer: {self.bias_on_hidden_layer.shape}")
-
+        derivative_Z1 = np.dot(self.weights_hidden_layer_to_output_layer, derivative_Z2) * (1 - np.power(self.hidden_layer, 2))
+        derivative_b1 = 1/m * np.sum(derivative_Z1, axis=1, keepdims=True)
         assert derivative_b1.shape == self.bias_on_hidden_layer.shape
-
-        derivative_W1 = cost_value_derivative*self.sigmoid(
-            self.output_layer_without_activity.T, derivative=True
-        ).dot(self.weights_hidden_layer_to_output_layer.T).dot(self.sigmoid(
-                self.hidden_layer_without_activity, derivative=True)
-        ).dot(x)
-
-        print(f"Derivative_W1: {derivative_W1.shape}, weights_input_layer_to_hidden_layer: {self.weights_input_layer_to_hidden_layer.shape}")
+        derivative_W1 = 1/m * np.dot(derivative_Z1, x).T
         assert derivative_W1.shape == self.weights_input_layer_to_hidden_layer.shape
 
         return derivative_W2, derivative_b2, derivative_W1, derivative_b1
@@ -121,12 +91,12 @@ class NeuralNetwork:
             derivative_b1,
             learning_rate):
 
-        self.weights_hidden_layer_to_output_layer -= learning_rate*derivative_W2
+        self.weights_hidden_layer_to_output_layer = self.weights_hidden_layer_to_output_layer -  learning_rate*derivative_W2
+        self.weights_input_layer_to_hidden_layer = self.weights_input_layer_to_hidden_layer -  learning_rate*derivative_W1
 
-        self.weights_input_layer_to_hidden_layer -= learning_rate*derivative_W1
-        self.bias_on_hidden_layer -= learning_rate*derivative_b1
+        self.bias_on_hidden_layer = self.bias_on_hidden_layer - learning_rate*derivative_b1
+        self.bias_on_output_layer = self.bias_on_output_layer - learning_rate*derivative_b2
 
-        self.bias_on_output_layer -= learning_rate*derivative_b2
 
     def train(self, x, y, learning_rate, mini_batch_size=500):
         x, y = np.array(x), np.array(y)
@@ -141,10 +111,15 @@ class NeuralNetwork:
                 derivative_W2, derivative_b2, derivative_W1, derivative_b1 = self.backpropagation(x, y)
                 self.gradient_descent(derivative_W2, derivative_b2, derivative_W1, derivative_b1, learning_rate)
 
+    def predict(self, x):
+        predicted_value = self.forward_propagation(x)
+
+        return predicted_value
+
 
 if __name__ == "__main__":
-    train_data = pd.read_csv("./dataset/MNIST/train.csv")
-    test_data = pd.read_csv("./dataset/MNIST/test.csv")
+    train_data = pd.read_csv("../datasets/MNIST/train.csv")
+    test_data = pd.read_csv("../datasets/MNIST/test.csv")
 
     train_data = train_data.sample(frac=1, random_state=1).reset_index()
 
@@ -154,3 +129,7 @@ if __name__ == "__main__":
     nl = NeuralNetwork()
 
     nl.train(X_train, y_train, 0.01)
+
+    prediction = nl.predict(test_data)
+
+